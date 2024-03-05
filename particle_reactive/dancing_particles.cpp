fbe7683c31614008bb90328558b5518eca18b9b5
yuehaogao <148832762+yuehaogao@users.noreply.github.com>
2024-03-05 11:58:33 -0800

    Accommodated the system to "distributed app"

--- particle_reactive/dancing_particles.cpp
+++ particle_reactive/dancing_particles.cpp
@@ -3,14 +3,17 @@
 // Audio-reactive visualizer
 //
 
-#include "al/app/al_App.hpp"
+//#include "al/app/al_App.hpp"
 #include "al/app/al_GUIDomain.hpp"
 //#include "al/ui/al_ControlGUI.hpp"
+#include "al/app/al_DistributedApp.hpp"
+#include "al/app/al_GUIDomain.hpp"
+#include "al_ext/statedistribution/al_CuttleboneDomain.hpp"
+#include "al_ext/statedistribution/al_CuttleboneStateSimulationDomain.hpp"
 #include "al/graphics/al_Shapes.hpp"
-#include "al/math/al_Functions.hpp"    // al::abs
+#include "al/math/al_Functions.hpp"
 #include "al/math/al_Random.hpp"
 #include "al/ui/al_Parameter.hpp"
-
 #include "Gamma/Analysis.h"
 #include "Gamma/DFT.h"
 #include "Gamma/Effects.h"
@@ -23,22 +26,44 @@ using namespace al;
 using namespace std;
 #define FFT_SIZE 4048
 
+
+// ??: can these constant stuff be passed into the distributed machines?J
+const float dragFactor = 0.15;
+const float particleMass = 3.0;
 const float sphereSize = 0.8;
+const float timeStep = 1.0;
 
 const int pattern1CylinderHeight = 70;
 const float pattern1CylinderHalfLength = 0.6;
 const int pattern1CylinderNumParticlePerlayer = 60;
 const float pattern1AngleIncrement = 2.0 * M_PI / pattern1CylinderNumParticlePerlayer;
 
-const int pattern2BallNumParticle = 1000;
+//const int pattern2BallNumParticle = 1000;
 
 // Common-used statements or parameters
 struct CommonState {
-  Vec3f pattern1Position[17000];
-  Vec3f pattern2Position[17000];
-  int size;
-  Nav nav;
+  // Public parameters for all patterns
+  int pattern;
+  float valueL;
+  float valueR;
+  float musicPower;
   float pointSize;
+  float springConstant;
+  vector<float> spectrum;
+
+  // Pattern 1 specific parameters
+  float pattern1CylinderRadius;
+  vector<Vec3f> pattern1Position;
+  vector<Vec3f> pattern1Velocity;
+  vector<Vec3f> pattern1Force;
+  vector<Vec3f> pattern1OriginalPosition;   // Archive of original positions of particles
+  vector<float> pattern1Mass;
+  
+  // Pattern 2 specific parameters
+  //Vec3f pattern2Position[5000];
+  //float sphereRadius;
+  //float repellingConstant;
+
 };
 
 // To generate a random 3D vector for position, color, ...
@@ -54,24 +79,18 @@ string slurp(string fileName);  // forward declaration
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 
-struct MyApp : App {
+struct MyApp : DistributedAppWithState<CommonState> {
   // ----------- All parameters Start -----------
   Parameter valueL{"value_left", 0, 0, 1};
   Parameter valueR{"value_right", 0, 0, 1};
-  Parameter pattern{"visual_pattern", 0, 0, 3};     // This will be limited to int only
-  Parameter musicPower{"/musicPower", "", 2.5, 0.5, 6.0};
+  Parameter pattern{"visual_pattern", 1, 0, 3};     // This will be limited to int only
+  Parameter musicPower{"/musicPower", "", 3.0, 0.1, 8.0};
   Parameter pointSize{"/pointSize", "", 0.5, 0.1, 1.5};
-  Parameter timeStep{"/timeStep", "", 1.0, 0.0, 1.5};
-  Parameter dragFactor{"/dragFactor", "", 0.15, 0.0, 0.6};
   Parameter springConstant{"/springConstant", "", 1.0, 0.1, 5.0};
   Parameter pattern1CylinderRadius{"/cylinderRadius", "", 0.25, 0.1, 1.0};
-  Parameter sphereRadius{"/sphereRadius", "", 2.0, 0.5, 4.0};
-  Parameter repellingConstant{"/repellingConstant", "", 0.0, 0.0, 0.01};
+  // Parameter sphereRadius{"/sphereRadius", "", 2.0, 0.5, 4.0};
+  // Parameter repellingConstant{"/repellingConstant", "", 0.0, 0.0, 0.01};
 
-  // The two spectrum tables for both channels
-  vector<float> spectrumL;
-  vector<float> spectrumR;
-  vector<float> spectrum;
 
   // STFT variables
   // Format of frequency samples: COMPLEX, MAG_PHASE, or MAG_FREQ
@@ -94,56 +113,56 @@ struct MyApp : App {
 
   // * Pattern 1: a cylinder of particles dancing according to the spectrum (enveloped)
   Mesh pattern1ParticleCylinder;
-  vector<Vec3f> pattern1Velocity;
-  vector<Vec3f> pattern1Force;
-  vector<Vec3f> pattern1OriginalPosition;   // Archive of original positions of particles
-  vector<float> pattern1Mass;
 
   // * Pattern 2:
-  //Mesh pattern2ParticleBall;
-  //Vec3f pattern2SphereCenter = {0.0, 0.0, 0.0};
+  // Mesh pattern2ParticleBall;
+  // Vec3f pattern2SphereCenter = {0.0, 0.0, 0.0};
 
   // * Pattern 3:
+  // ------------------------------------
 
-  // ----------- All Meshes -------------
 
   // onInit
   // When the app is first initialized
   //
   void onInit() override {
-    // Load the sound file
-    player.load("../tv.mp3");
-    
-    // set up GUI
-    auto GUIdomain = GUIDomain::enableGUI(defaultWindowDomain());
-    auto& gui = GUIdomain->newGUI();
-    gui.add(valueL);
-    parameterServer() << valueL;
-    gui.add(valueR);
-    parameterServer() << valueR;
-    gui.add(pattern);
-    parameterServer() << pattern;
-    gui.add(musicPower);
-    parameterServer() << musicPower;
-    gui.add(pointSize);
-    parameterServer() << pointSize;
-    gui.add(timeStep);
-    parameterServer() << timeStep;
-    gui.add(dragFactor);
-    parameterServer() << dragFactor;
-    gui.add(springConstant); 
-    parameterServer() << springConstant;
-    gui.add(pattern1CylinderRadius);
-    parameterServer() << pattern1CylinderRadius;
-    gui.add(sphereRadius);
-    parameterServer() << sphereRadius;
-    gui.add(repellingConstant);
-    parameterServer() << repellingConstant;
 
-    // Declare the size of the spectrums
-    spectrumL.resize(FFT_SIZE / 2 + 1);
-    spectrumR.resize(FFT_SIZE / 2 + 1);
-    spectrum.resize(FFT_SIZE / 2 + 1);
+    // Try starting the program
+    auto cuttleboneDomain =
+        CuttleboneStateSimulationDomain<CommonState>::enableCuttlebone(this);
+    if (!cuttleboneDomain) {
+      std::cerr << "ERROR: Could not start Cuttlebone. Quitting." << std::endl;
+      quit();
+    }
+    
+
+    if (isPrimary()) {
+      // Load the sound file
+      player.load("../tv.mp3");
+    
+      // set up GUI
+      auto GUIdomain = GUIDomain::enableGUI(defaultWindowDomain());
+      auto& gui = GUIdomain->newGUI();
+      gui.add(valueL);
+      state().valueL = valueL;
+      gui.add(valueR);
+      state().valueR = valueR;
+      gui.add(pattern);
+      state().pattern = pattern;
+      gui.add(musicPower);
+      state().musicPower = musicPower;
+      gui.add(pointSize);
+      state().pointSize = pointSize;
+      gui.add(springConstant);
+      state().springConstant = springConstant;
+      gui.add(pattern1CylinderRadius);
+      state().pattern1CylinderRadius = pattern1CylinderRadius;
+      // gui.add(sphereRadius);
+      // gui.add(repellingConstant);
+
+      // Declare the size of the spectrums
+      state().spectrum.resize(FFT_SIZE / 2 + 1);
+    }
 
   }
 
@@ -154,52 +173,56 @@ struct MyApp : App {
   // Initialize the position of camera
   //
   void onCreate() override { 
-    pointShader.compile(slurp("../point-vertex.glsl"),
-                        slurp("../point-fragment.glsl"),
-                        slurp("../point-geometry.glsl"));
+    if (isPrimary()) {
+      pointShader.compile(slurp("../point-vertex.glsl"),
+                          slurp("../point-fragment.glsl"),
+                          slurp("../point-geometry.glsl"));
 
-    // ----------- Initialize Parameters for All Meshes Begins -------------
+      // ----------- Initialize Parameters for All Meshes Begins -------------
 
-    // Pattern 0: move the two spheres seperately to the left and right
-    addSphere(pattern0SphereL);
-    addSphere(pattern0SphereR);
+      // Pattern 0: move the two spheres seperately to the left and right
+      addSphere(pattern0SphereL);
+      addSphere(pattern0SphereR);
 
-    // Pattern 1: create all the particles with their initial positions, colors, velocity, and forces
-    pattern1ParticleCylinder.primitive(Mesh::POINTS);
+      // Pattern 1: create all the particles with their initial positions, colors, velocity, and forces
+      pattern1ParticleCylinder.primitive(Mesh::POINTS);
   
-    for (int layerIndex = 0; layerIndex < pattern1CylinderHeight; layerIndex++) {
-      float y = -1.0 * pattern1CylinderHalfLength + (2.0 * pattern1CylinderHalfLength / pattern1CylinderHeight) * layerIndex;
+      for (int layerIndex = 0; layerIndex < pattern1CylinderHeight; layerIndex++) {
+        float y = -1.0 * pattern1CylinderHalfLength + (2.0 * pattern1CylinderHalfLength / pattern1CylinderHeight) * layerIndex;
       
-      for (int particleIndex = 0; particleIndex < pattern1CylinderNumParticlePerlayer; particleIndex++) {
-        float angle = particleIndex * pattern1AngleIncrement;
-        float x = pattern1CylinderRadius * cos(angle);
-        float z = pattern1CylinderRadius * sin(angle);
-        pattern1ParticleCylinder.vertex(Vec3f(x, y, z));
-        pattern1OriginalPosition.push_back(Vec3f(x, y, z));  // Archive of original positions of particles
+        for (int particleIndex = 0; particleIndex < pattern1CylinderNumParticlePerlayer; particleIndex++) {
+          float angle = particleIndex * pattern1AngleIncrement;
+          float x = pattern1CylinderRadius * cos(angle);
+          float z = pattern1CylinderRadius * sin(angle);
 
-        float hue = 0.7 / pattern1CylinderHeight * layerIndex;
-        pattern1ParticleCylinder.color(HSV(hue, 1.0f, 1.0f));
+          // Place the particle's position
+          pattern1ParticleCylinder.vertex(Vec3f(x, y, z));
+          state().pattern1OriginalPosition.push_back(Vec3f(x, y, z));  // Archive of original positions of particles
 
-        pattern1Mass.push_back(3.0);
-        pattern1ParticleCylinder.texCoord(pow(3.0, 1.0f / 3), 0);
-        pattern1Velocity.push_back(Vec3f(0.0, 0.0, 0.0));
-        pattern1Force.push_back(Vec3f(0.0, 0.0, 0.0));
-      
+          // Color the particle
+          float hue = 0.7 / pattern1CylinderHeight * layerIndex;
+          pattern1ParticleCylinder.color(HSV(hue, 1.0f, 1.0f));
+
+          // Set the particle's physical force system
+          state().pattern1Mass.push_back(particleMass);
+          pattern1ParticleCylinder.texCoord(pow(particleMass, 1.0f / 3), 0);
+          state().pattern1Velocity.push_back(Vec3f(0.0, 0.0, 0.0));
+          state().pattern1Force.push_back(Vec3f(0.0, 0.0, 0.0));
+        }
       }
-    }
-    //const vector<Vec3f> &pattern1OriginalPositionVec(pattern1ParticleCylinder.vertices());
-
-    // Pattern 2: ...
-    // Pattern 3: ...
     
-    // ----------- Initialize Parameters for All Meshes Ends -------------
+      // Pattern 2: ...
+      // Pattern 3: ...
+    
+      // ----------- Initialize Parameters for All Meshes Ends -------------
   
-    // Set the magnitude of the envelop followers
-    followLeft.lag(0.5); 
-    followRight.lag(0.5);
+      // Set the magnitude of the envelop followers
+      followLeft.lag(0.5); 
+      followRight.lag(0.5);
   
-    // Initialize the position of "camera"
-    nav().pos(0, 0, 2.5);
+      // Initialize the position of "camera"
+      nav().pos(0, 0, 2.5);
+    }
   }
 
   // onSound
@@ -217,29 +240,16 @@ struct MyApp : App {
       io.out(0) = sLeft;
       io.out(1) = sRight;
 
-      valueL.set(followLeft(sLeft));
-      valueR.set(followRight(sRight));
+      valueL = (followLeft(sLeft));
+      valueR = (followRight(sRight));
 
       // STFT
-      ///*
       if (stft(io.out(0))) { 
         // Loop through all the frequency bins
-        //int testing = 0;
         for (unsigned k = 0; k < stft.numBins(); ++k) {
-          spectrum[k] = 8.0 * tanh(pow(stft.bin(k).real(), 1.5));
-          //testing++;
-        }
-        //cout << testing;
-      }
-      /*
-      if (stft(io.out(1))) { 
-        // Loop through all the frequency bins
-        for (unsigned k = 0; k < stft.numBins(); ++k) {
-          spectrumR[k] = 10.0 * tanh(pow(stft.bin(k).real(), 1.5) );
+          state().spectrum[k] = 8.0 * tanh(pow(stft.bin(k).real(), 1.5));
         }
       }
-      */
-      //*/
 
       // https://github.com/adamstark/Gist
     }
@@ -251,38 +261,39 @@ struct MyApp : App {
   //
   void onDraw(Graphics& g) override {
     // The background changes brightness according to the volume
-    g.clear(0.6 * (valueL.get() + valueR.get()) * (valueL.get() + valueR.get()) * 2.0 * musicPower);
-    float redColorChange = 5.0 * valueL.get();
+    float valueLAndR = state().valueL + state().valueR;
+    g.clear(1.2 * pow(valueLAndR, 2.0) * state().musicPower);
+    float redColorChange = 5.0 * state().valueL;
     if (redColorChange > 2.0) {
       redColorChange = 2.0;
     }
-    float blueColorChange = 5.0 * valueR.get();
-    if (blueColorChange > 2.0) {
-      blueColorChange = 2.0;
+    float greenColorChange = 5.0 * state().valueR;
+    if (greenColorChange > 2.0) {
+      greenColorChange = 2.0;
     } 
 
     // Show different pattern according to the "pattern" value
-    switch((int)pattern.get()) {
+    switch((int)(state().pattern)) {
       case 0:
         g.pushMatrix();
         g.translate(-0.5, 0, 0);
-        g.scale(sphereSize * valueL.get() * musicPower);
+        g.scale(sphereSize * state().valueL * state().musicPower);
         //g.lighting(true);
-        g.color(RGB(1.0, 1.0 - redColorChange * redColorChange, 1.0 - redColorChange * redColorChange));
+        g.color(RGB(1.0, 1.0 - pow(redColorChange, 2.0), 1.0 - pow(redColorChange, 2.0)));
         g.draw(pattern0SphereL);
         g.popMatrix();
 
         g.pushMatrix();
         g.translate(0.5, 0, 0);
-        g.scale(sphereSize * valueR.get() * musicPower);
+        g.scale(sphereSize * state().valueR * state().musicPower);
         //g.lighting(true);
-        g.color(RGB(1.0 - blueColorChange * blueColorChange, 1.0, 1.0 - blueColorChange * blueColorChange));
+        g.color(RGB(1.0 - pow(greenColorChange, 2.0), 1.0, 1.0 - pow(greenColorChange, 2.0)));
         g.draw(pattern0SphereR);
         g.popMatrix();
         break;
       case 1:
         g.shader(pointShader);
-        g.shader().uniform("pointSize", pointSize / 100);
+        g.shader().uniform("pointSize", state().pointSize / 100);
         g.blending(true);
         g.blendTrans();
         g.depthTesting(true);
@@ -301,50 +312,63 @@ struct MyApp : App {
   // For pattern 2, 3: calculate the new force excerted on each particle
   //
   void onAnimate(double dt) override {
-    // Set the parameter of "pattern" to its floored-down int value
-    int flooredPatternIndex = (int) (std::floor(pattern));
-    pattern = flooredPatternIndex;
+    if (isPrimary()) {
+      // Set the parameter of "pattern" to its floored-down int value
+      //int flooredPatternIndex = (int) (std::floor(state().pattern));
+      //pattern = flooredPatternIndex;
 
-    // Pattern 1:
-    // Deal with all the particle forces
-    vector<Vec3f> &pattern1PositionVec(pattern1ParticleCylinder.vertices());
-    for (int i = 0; i < pattern1PositionVec.size(); i++) {
-      // Add spring force to particles' original positions
-      Vec3f centerAtItsLayer = {0.0, pattern1PositionVec[i].y, 0.0};
-      Vec3f particleToCenter = centerAtItsLayer - pattern1PositionVec[i];
-      float distanceToSurface = particleToCenter.mag() - pattern1CylinderRadius;
-      Vec3f springForce = particleToCenter.normalize() * springConstant * distanceToSurface;
-      pattern1Force[i] += springForce;
-    }
-    
-    // Exert the force and accelerations
-    for (int i = 0; i < pattern1Velocity.size(); i++) {
-      pattern1Force[i] += - pattern1Velocity[i] * dragFactor;
-
-      // Important: add the force excerted by audio
-      float musicForce = 0.0;
+      // Unify local data with "state" data (only pattern)
+      state().pattern = pattern;
       
-      int positionInSpectrum = std::floor(pow(i, 1.7) / 5000);
-      //int positionInSpectrum = std::floor(((pattern1PositionVec[i].y + 0.6) / 1.2) * 2025);
-      //int positionInSpectrum = (int) (FFT_SIZE * 0.5 * ((pattern1PositionVec[i].y + pattern1CylinderHalfLength) / (2 * pattern1CylinderHalfLength))) - 0.5 * FFT_SIZE;
-      /*
-      if (pattern1OriginalPosition[i].x < 0) {
-        musicForce = spectrumL[positionInSpectrum];
-      } else {
-        musicForce = spectrumR[positionInSpectrum];
+      // Pattern 1:
+      // Deal with all the particle forces
+      vector<Vec3f> &pattern1PositionVec(pattern1ParticleCylinder.vertices());
+      for (int i = 0; i < pattern1PositionVec.size(); i++) {
+        // Add spring force to particles' original positions
+        Vec3f centerAtItsLayer = {0.0, pattern1PositionVec[i].y, 0.0};
+        Vec3f particleToCenter = centerAtItsLayer - pattern1PositionVec[i];
+        float distanceToSurface = particleToCenter.mag() - state().pattern1CylinderRadius;
+        Vec3f springForce = particleToCenter.normalize() * state().springConstant * distanceToSurface;
+        state().pattern1Force[i] += springForce;
       }
-      */
-      musicForce = spectrum[positionInSpectrum];
-      pattern1Force[i] += Vec3f(pattern1OriginalPosition[i].x, 0.0, pattern1OriginalPosition[i].z) * musicForce * musicPower * ((200.0 + 3.0 * i) / 2000.0);
-
-      pattern1Velocity[i] += pattern1Force[i] / pattern1Mass[i] * timeStep;
-      pattern1PositionVec[i] += pattern1Velocity[i] * timeStep;
-    }
-
-
-    // Clear all accelerations
-    for (auto &a : pattern1Force) a.set(0);
     
+      // Exert the force and accelerations
+      for (int i = 0; i < state().pattern1Velocity.size(); i++) {
+        state().pattern1Force[i] -= state().pattern1Velocity[i] * dragFactor;
+
+        // Important: add the force excerted by audio
+        float musicForce = 0.0;
+      
+        int positionInSpectrum = std::floor(pow(i, 1.7) / 5000);
+        //int positionInSpectrum = std::floor(((pattern1PositionVec[i].y + 0.6) / 1.2) * 2025);
+        //int positionInSpectrum = (int) (FFT_SIZE * 0.5 * ((pattern1PositionVec[i].y + pattern1CylinderHalfLength) / (2 * pattern1CylinderHalfLength))) - 0.5 * FFT_SIZE;
+
+        musicForce = state().spectrum[positionInSpectrum];
+        state().pattern1Force[i] += Vec3f(state().pattern1OriginalPosition[i].x, 0.0, state().pattern1OriginalPosition[i].z) * musicForce * state().musicPower * ((200.0 + 3.0 * i) / 2000.0);
+
+        state().pattern1Velocity[i] += state().pattern1Force[i] / state().pattern1Mass[i] * timeStep;
+        pattern1PositionVec[i] += state().pattern1Velocity[i] * timeStep;
+      }
+
+      // Clear all accelerations
+      for (auto &a : state().pattern1Force) a.set(0);
+
+      // Unify other parameters between local and "state"
+      state().valueL = valueL;
+      state().valueR = valueR;
+      state().pointSize = pointSize;
+      state().musicPower = musicPower;
+      state().springConstant = springConstant;
+      state().pattern1CylinderRadius = pattern1CylinderRadius;
+
+    } else {
+
+      // ??? this part is SOOOOOOOOO confusing
+      std::cout << state().pattern << " " << state().valueL << " " << state().valueR << state().musicPower << " " << state().pointSize << " " << state().springConstant << std::endl;
+      //std::cout << state().spectrum << std::endl;
+      std::cout << state().pattern1CylinderRadius << std::endl;
+      //std::cout << state().pattern1Position << " " << state().pattern1Velocity << " " << state().pattern1Force << " " << state().pattern1OriginalPosition << " " << state().pattern1Mass << std::endl;;
+    }
 
   }
 
