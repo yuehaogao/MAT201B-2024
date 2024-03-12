11afa77caafce59c7c5bdca9e8b49d3049b3a860
yuehaogao <148832762+yuehaogao@users.noreply.github.com>
2024-03-12 14:43:54 -0700

    Update dancing_particles.cpp

--- particle_reactive/dancing_particles.cpp
+++ particle_reactive/dancing_particles.cpp
@@ -458,7 +458,7 @@ struct MyApp : DistributedAppWithState<CommonState> {
       }
 
 
-      pointSize = 0.12 + musicPower * 0.3 * pow((valueL + valueR), 0.8);
+      pointSize = 0.12 + musicPower * 0.3 * pow((valueL + valueR), 0.6);
 
       // Clear all accelerations
       for (auto &a : pattern1Force) a.set(0);
